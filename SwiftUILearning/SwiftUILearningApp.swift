f4ceef4e6fbdd28066891e6b5e10b4cb94f0be92
aksilont <aksilont@gmail.com>
2021-10-21 21:01:11 +0300

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Aksilont on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
